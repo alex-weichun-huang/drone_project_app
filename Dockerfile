bbf3fbc417d7a9f9d3f1ac76e92329bf93229af9
Alex Huang <whuang288@wisc.edu>
2023-11-14 23:31:22 -0600

    built simple flask app

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM ubuntu:23.10
-WORKDIR /app
-COPY . /app
-RUN apt-get update && apt-get install -y unzip python3 python3-pip
-RUN python3 -m pip install -r /app/requirements.txt --break-system-packages
-RUN python3 -m pip install -e /app/detectron2 --break-system-packages
-EXPOSE 8501
-CMD ["streamlit", "run", "--server.address", "0.0.0.0", "/app/app.py"]
--- app.py
+++ app.py
@@ -1,35 +1,84 @@
 import os
-import sys
-import cv2
+import io
+import uuid
+import shutil
 import glob
-import torch
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-from pathlib import Path
-from utils import load_model, inference
 import warnings
+from flask import Flask, request, render_template, redirect, url_for
+from PIL import Image
+from utils import load_model, inference
+
 warnings.filterwarnings("ignore")
 
+app = Flask(__name__)
 device = "cpu"
 model = load_model("./assets/model", "model_ckpt.pth", device=device)
 
-# Streamlit application main function
-def main():
-    st.title("Detect Marching Band Members!")
 
-    # File uploader widget
-    uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
-    if uploaded_file is not None:
-        
-        # Open and display the image
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-        # add bounding boxes to the image
-        box_image = inference(model, image, score_thresh=0.75, device=device)
-        st.image(box_image, caption='Box Image', use_column_width=True)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
 
-if __name__ == "__main__":
-    main()
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        
+        if file and allowed_file(file.filename):
+            image = Image.open(file.stream)
+            box_image = inference(model, image, score_thresh=0.75, device=device)
+
+            # Generate a unique filename and save the image temporarily
+            temp_filename = f"image_{uuid.uuid4()}.jpg"
+            box_image.save(os.path.join('static', temp_filename))
+            
+            # Pass the filename to the template
+            return render_template('inference.html', image_path=temp_filename)
+
+    elif request.method == 'GET':
+        # remove temporary images
+        temp_images = glob.glob(os.path.join('static', 'image_*.jpg'))
+        for image in temp_images:
+            os.remove(image)
+            
+        # Get all images from the db folder
+        db_images = glob.glob(os.path.join('static', 'db', '*.jpg'))
+        return render_template('index.html', images=db_images)
+    
+    return render_template('index.html')
+
+
+
+@app.route('/save_image/<filename>', methods=['POST']) 
+def save_image(filename):
+
+    src = os.path.join('static', filename)
+    dest = os.path.join('static', 'db', filename)
+
+    try:
+        shutil.move(src, dest)
+        print(f"Saving image {filename} to db")
+        return redirect(url_for('index'))
+    except Exception as e:
+        print(f"Error saving {filename}: {e}")
+        return "Error saving image", 500
+    
+
+@app.route('/remove_image/<filename>', methods=['DELETE'])
+def remove_image(filename):
+    file_path = os.path.join('static', 'db', filename)
+    try:
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            return {"message": f"{filename} deleted successfully"}, 200
+        else:
+            return {"message": f"{filename} not found"}, 404
+    except Exception as e:
+        return {"message": str(e)}, 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
