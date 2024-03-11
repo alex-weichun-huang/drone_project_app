bf69c7504f8c654857898c103183b9dd05032f2a
Alex <whuang288@wisc.edu>
2024-03-11 02:31:45 +0000

    cleaned detectron2 dependencies

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ WORKDIR /app
 COPY . /app
 RUN apt-get update && apt-get install -y unzip python3 python3-pip
 RUN python3 -m pip install -r /app/requirements.txt --break-system-packages
-RUN python3 -m pip install -e /app/detectron2 --break-system-packages
 EXPOSE 8501
 CMD ["streamlit", "run", "--server.address", "0.0.0.0", "/app/app.py"]
--- app.py
+++ app.py
@@ -1,22 +1,91 @@
 import os
 import sys
-import cv2
-import glob
-import torch
-import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-from pathlib import Path
-from utils import load_model, inference
 import warnings
 warnings.filterwarnings("ignore")
+dir_path = os.path.join(os.path.dirname(__file__), "detectron2")
+sys.path.append(dir_path)
 
-device = "cpu"
-model = load_model("./assets/model", "model_ckpt.pth", device=device)
 
-# Streamlit application main function
-def main():
+import cv2
+import torch
+import numpy as np
+import streamlit as st
+from PIL import Image
+from torchvision.ops import nms 
+from detectron2.config import get_cfg
+from detectron2.modeling import build_model
+from detectron2.checkpoint import DetectionCheckpointer
+
+
+def load_model(model_dir, model_name, device="cpu"):
+    """ 
+    Load model from model_dir/model_name.
+    """
+    # load config from yaml file
+    cfg = get_cfg()
+    yaml_file = os.path.join(model_dir, 'config.yaml')
+    with open(yaml_file, 'r') as f:
+        yaml_string = f.read()
+        cfg = cfg.load_cfg(yaml_string)
+        cfg.MODEL.WEIGHTS = os.path.join(model_dir, model_name)
+        cfg.MODEL.DEVICE = device
+
+    # build the model and load checkpoint
+    model = build_model(cfg)
+    _ = model.eval()
+    checkpointer = DetectionCheckpointer(model)
+    _ = checkpointer.load(cfg.MODEL.WEIGHTS)
+    
+    return model
+
+
+def preprocess_image(pil_image, device="cpu"):
+    """ 
+    Preprocess image for inference.
+    """
+    tensor_image = torch.as_tensor(np.array(pil_image).transpose(2, 0, 1).astype("float32")).to(device)
+    image_dict = {"image": tensor_image, "height": tensor_image.shape[1], "width": tensor_image.shape[2]}
+    return image_dict
+
+
+def nms_all_classes(instances, iou_thresh):
+    """ 
+    Apply non-maximum suppression to inference instances regardless of class.
+    """
+    valid_ind = nms(instances.pred_boxes.tensor, instances.scores, iou_thresh)
+    instances.pred_boxes.tensor = instances.pred_boxes.tensor[valid_ind]
+    instances.scores = instances.scores[valid_ind]
+    instances.pred_classes = instances.pred_classes[valid_ind]
+    return instances
+
+
+def inference(model, pil_image, output_path=None, score_thresh=0.75, device="cpu"):
+    """
+    Perform inference on a single image.
+    """
+    np_image = np.array(pil_image)
+    image_dict = preprocess_image(pil_image, device=device)
+    
+    with torch.no_grad():
+        detections = model([image_dict])[0]['instances']
+        detections = nms_all_classes(detections, 0.05)
+        boxes = detections.get_fields()["pred_boxes"].tensor.cpu().numpy()
+        scores = detections.get_fields()["scores"].cpu().numpy()
+        for box, score in zip(boxes, scores):
+            if score >= score_thresh:
+                cv2.rectangle(np_image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
+        
+        PIL_image = Image.fromarray(np_image)  
+        if output_path is not None:
+            PIL_image.save(output_path)
+    
+    return PIL_image
+
+
+def streamlit_app(model, device):
+    """
+    Streamlit application main function
+    """
     st.title("Detect Marching Band Members!")
 
     # File uploader widget
@@ -31,5 +100,8 @@ def main():
         box_image = inference(model, image, score_thresh=0.75, device=device)
         st.image(box_image, caption='Box Image', use_column_width=True)
 
+
 if __name__ == "__main__":
-    main()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = load_model("./assets/model", "model_ckpt.pth", device=device)
+    streamlit_app(model, device)
